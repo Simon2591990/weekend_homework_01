5c1147acffd7302b0090ca55349b58af751c8833
Simon Charters <simon259@hotmail.co.uk>
2020-11-20 13:55:14 +0000

    up to question 8

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,6 +1,28 @@
 # WRITE YOUR FUNCTIONS HERE
+import pdb
 def get_pet_shop_name(shop):
     return shop["name"]
 
 def get_total_cash(shop):
     return shop["admin"]["total_cash"]
+
+def add_or_remove_cash(shop, sale):
+    shop["admin"]["total_cash"] += sale
+
+def get_pets_sold(shop):
+    return shop["admin"]["pets_sold"]
+
+def increase_pets_sold(shop, pets_sold):
+    shop["admin"]["pets_sold"] += pets_sold
+
+def get_stock_count(shop):
+    return len(shop["pets"])
+
+def get_pets_by_breed(shop, breed):
+    
+    list_of_specified_breed = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed:
+            list_of_specified_breed.append(pet["name"])
+            
+    return list_of_specified_breed
