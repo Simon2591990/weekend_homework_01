aabf160fa48991e6157a1021ffb08a1cc0d70246
Simon Charters <simon259@hotmail.co.uk>
2020-11-20 14:25:15 +0000

    MVP done

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -39,3 +39,15 @@ def remove_pet_by_name(shop, name):
 
 def add_pet_to_stock(shop, new_pet):
     shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
